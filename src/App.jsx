3179b020aa197b6a1d1fe2f8252fa594f2b62f2c
zerow.kim <0wkim@naver.com>
2026-02-04 12:55:21 +0900

    chore: 프로젝트 초기 설정 with react + vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/* <Route path="/" element={<Home />} />  */}
+    </div>
+  )
+}
+
+export default App;
